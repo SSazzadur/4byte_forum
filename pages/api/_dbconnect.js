8d7b7bbb27a526c306802e147cd5a8749471a855
SSahil <ssasahil000@gmail.com>
2021-11-09 19:30:56 +0530

    connected-to-a-online-server

--- pages/api/_dbconnect.js
+++ pages/api/_dbconnect.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "4byte_forum",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect();
--- pages/api/user/userauth.js
+++ pages/api/user/userauth.js
@@ -9,21 +9,23 @@ export default function userauth(req, res) {
         const token = authHeader && authHeader.split(" ")[1];
 
         if (token == null) res.json({ isAuth: false });
+        else {
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+                if (err) res.json({ status: "error", isAuth: false });
+                else {
+                    const sql = `SELECT * FROM users WHERE user_email='${user.email}'`;
 
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) res.json({ status: "error", isAuth: false });
-
-            const sql = `SELECT * FROM users WHERE user_email='${user.email}'`;
-
-            db.query(sql, (error, result) => {
-                if (error) res.json({ isAuth: false });
-                else if (result.length > 0)
-                    res.status(200).json({
-                        isAuth: true,
-                        data: user,
+                    db.query(sql, (error, result) => {
+                        if (error) res.json({ isAuth: false });
+                        else if (result.length > 0)
+                            res.status(200).json({
+                                isAuth: true,
+                                data: user,
+                            });
+                        else res.json({ isAuth: false });
                     });
-                else res.json({ isAuth: false });
+                }
             });
-        });
+        }
     }
 }
